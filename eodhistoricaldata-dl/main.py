3e094425610fb0028eaa8e794a1dc3ab58bc0f76
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-04-05 00:13:17 -0400

    added api limiter

--- eodhistoricaldata-dl/main.py
+++ eodhistoricaldata-dl/main.py
@@ -17,29 +17,31 @@ try:
     # If we are in the middle of another download
     with open("output/usage.json", "r") as f:
         usage = json.load(f)
+    
+    last_request = datetime.datetime.strptime(usage["dt"], "%Y-%m-%d %H:%M:%S.%f")
+    
+    if (datetime.datetime.now() - last_request).total_seconds() < 86400 and usage["count"] > 10000:
+        print("Already downloaded today")
+        exit()
 except:
-    # if this is the first time we are running this script
+    # First time we are running this script
     now = datetime.datetime.now()
-    usage = {"dt": str(now), "req": 0}
+    usage = {"dt": str(now), "count": 0}
 
     with open("output/usage.json", "w") as f:
         json.dump(usage, f)
 
-last_request = datetime.datetime.strptime(usage["dt"], "%Y-%m-%d %H:%M:%S.%f")
-total_requests = usage["req"]
+total_requests = usage["count"]
 
-# if last_request was more than 24 hours ago, reset the counter
-if (datetime.datetime.now() - last_request).total_seconds() > 86400:
-    total_requests = 0
-else:
-    quit()
-    
-print(1)
-
-# get a list of all tickers
-exchange_code = "US"
-#tickers_df = endpoints.get_all_tickers(exchange_code, api_key)
-#tickers_df.to_csv("output/tickers.csv", index=False)
+# see if tickers.csv exists in output
+try:
+    with open("output/tickers.csv", "r") as f:
+        pass
+except:
+    # get a list of all tickers
+    exchange_code = "US"
+    tickers_df = endpoints.get_all_tickers(exchange_code, api_key)
+    tickers_df.to_csv("output/tickers.csv", index=False)
 
 # create a tracking file
 tracker = pd.read_csv("output/tickers.csv")
@@ -51,30 +53,40 @@ tracker = tracker.reset_index(drop=True)
 
 tracker.to_csv("output/tracker.csv", index=False)
 
-print(tracker.head())
-print(tracker.shape)
-
 # get data for each ticker
-for index, row in tracker.iterrows():
-    ticker = row['Code'] + ".US"
-    
-    try:
-        os.mkdir(f"output/{index}")
-    except:
-        pass # already exists
+while usage["count"] < 10000:
 
-    if row['retrieved_fundamentals'] == 0:
-        fundamentals = endpoints.get_fundamentals(ticker, api_key)
+    for index, row in tracker.iterrows():
+        ticker = row['Code'] + ".US"
+        
+        try:
+            os.mkdir(f"output/{index}")
+        except:
+            pass # already exists
+        
+        if row['retrieved_fundamentals'] == 0:
+            fundamentals = endpoints.get_fundamentals(ticker, api_key)
 
-        with open(f"output/{index}/fundamentals.json", "w") as f:
-            json.dump(fundamentals, f)
+            with open(f"output/{index}/fundamentals.json", "w") as f:
+                json.dump(fundamentals, f)
 
-        tracker.at[index, 'retrieved_fundamentals'] = 1
+            tracker.at[index, 'retrieved_fundamentals'] = 1
+            usage["count"] += 1
+            
+            tracker.to_csv("output/tracker.csv", index=False)
+        
+            with open("output/usage.json", "w") as f:
+                json.dump(usage, f)
 
-    if row['retrieved_price'] == 0:
-        eod_prices = endpoints.get_eod_prices(ticker, api_key)
-        eod_prices.to_csv(f"output/{index}/prices.csv", index=False)
+        
+        if row['retrieved_price'] == 0:
+            eod_prices = endpoints.get_eod_prices(ticker, api_key)
+            eod_prices.to_csv(f"output/{index}/prices.csv", index=False)
 
-        tracker.at[index, 'retrieved_price'] = 1
-    
-    tracker.to_csv("output/tracker.csv", index=False)
+            tracker.at[index, 'retrieved_price'] = 1
+            usage["count"] += 1
+
+            tracker.to_csv("output/tracker.csv", index=False)
+
+            with open("output/usage.json", "w") as f:
+                json.dump(usage, f)
