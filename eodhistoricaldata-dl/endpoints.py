e31f95354505d4bbfa5bb94148ffb51eff3ca9f4
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-04-04 21:06:00 -0400

    added get_all_tickers

--- eodhistoricaldata-dl/endpoints.py
+++ eodhistoricaldata-dl/endpoints.py
@@ -1,15 +1,18 @@
+import io 
 import requests
+import pandas as pd
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 api_key = config["API_KEY"]
 
-
 def get_all_tickers(exchange_code, api_key):
     url = f"https://eodhistoricaldata.com/api/exchange-symbol-list/{exchange_code}?api_token={api_key}"
     r = requests.get(url)
 
-    print(r.text)
+    df = pd.read_csv( io.StringIO(r.text) )
+
+    df.to_csv("tickers.csv")
 
 exchange_code = "US"
 get_all_tickers(exchange_code, api_key)
